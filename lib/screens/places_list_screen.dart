17b73f27b010989911cd41940d86c897579eb7e4
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2020-12-16 18:22:54 -0400

    Implementando a tela de listagem dos dados.

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places_flutter/providers/great_places.dart';
 import 'package:great_places_flutter/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlaceListScreen extends StatelessWidget {
   @override
@@ -16,8 +18,25 @@ class PlaceListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: Text(
+            'Nenhum local cadastrado!',
+          ),
+        ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.itemsCount == 0
+            ? ch
+            : ListView.builder(
+                itemCount: greatPlaces.itemsCount,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        FileImage(greatPlaces.itemByIndex(i).image),
+                  ),
+                  title: Text(greatPlaces.itemByIndex(i).title),
+                  onTap: () {},
+                ),
+              ),
       ),
     );
   }
